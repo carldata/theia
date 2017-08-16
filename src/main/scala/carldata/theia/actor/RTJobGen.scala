a29d9a6b67da7b0be6ac98cbe6dc1f527b063419
Krzysztof Langner <klangner@gmail.com>
2017-08-16 15:14:59 +0200

    Load RTJob from external file

--- src/main/scala/carldata/theia/actor/RTJobGen.scala
+++ src/main/scala/carldata/theia/actor/RTJobGen.scala
@@ -1,10 +1,14 @@
 package carldata.theia.actor
 
+import java.io.File
+
 import akka.actor.{Actor, ActorRef, Props}
 import carldata.hs.RealTime.RealTimeJsonProtocol._
 import carldata.hs.RealTime.{AddAction, RealTimeRecord}
 import carldata.theia.actor.Messages.{KMessage, Tick}
 import spray.json._
+
+import scala.io.Source
 
 
 /** Generate jobs which should run in real time. */
@@ -14,20 +18,29 @@ object RTJobGen {
 
 class RTJobGen(sinkActor: ActorRef) extends Actor {
 
-  /** Id script. Return the same value */
-  val script: String =
-    """
-      |module Theia1
-      |
-      |def main(t: DateTime, v: Number): Number = v+1
-    """.stripMargin
+  val jobs: Seq[RealTimeRecord] = {
+    new File("config")
+      .listFiles
+      .filter(x => x.isFile && x.getName.endsWith(".rt"))
+      .flatMap(f => parseConfig(f).toList)
+  }
 
   def receive: Actor.Receive = {
 
     case Tick =>
-      val r = RealTimeRecord(AddAction, "theia-1", script, "theia-in-1", "theia-out-1")
-      println("Send RealTime Job on channel" + r.trigger)
-      sinkActor ! KMessage("theia", r.toJson.compactPrint)
+      jobs.foreach { j =>
+        println("Send RealTime Job on channel " + j.trigger)
+        sinkActor ! KMessage("theia", j.toJson.compactPrint)
+      }
 
+  }
+
+  def parseConfig(f: File): Option[RealTimeRecord] = {
+    val lines = Source.fromFile(f).getLines().filter(!_.startsWith("#")).toList
+    val calculationId = "theia-1"
+    val trigger = lines.headOption.getOrElse("")
+    val output = lines.drop(1).headOption.getOrElse("")
+    val script = lines.drop(2).mkString("\n")
+    Some(RealTimeRecord(AddAction, calculationId, script, trigger, output))
   }
 }
