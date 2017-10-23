e06c61a12ebe7676daa48dd1944b72b44c2d2082
Tomasz Pichlak <tpichlak@gmail.com>
2017-10-23 13:06:13 +0200

    Implemented graylog functionality for logback.

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-java -jar /root/theia.jar --kafka=$Kafka_Broker --eps=$Events_Per_Second
+java -Dlogback.configurationFile=/root/logback_deployment.xml  -jar /root/theia.jar --kafka=$Kafka_Broker --eps=$Events_Per_Second
 
