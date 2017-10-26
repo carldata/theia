c7f4449b2b2dd9cd7acd280617d8f4577f4eaba4
wenta <jakubwenta@gmail.com>
2017-10-26 13:36:23 +0200

    Update parameter name.

--- etc/entrypoint.sh
+++ etc/entrypoint.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-java -Dlogback.configurationFile=/root/logback_deployment.xml  -jar /root/theia.jar --kafka=$Kafka_Broker --eps=$Events_Per_Second  --cps=$Channels_Per_Second --statSDHost=$StatSD_Host
+java -Dlogback.configurationFile=/root/logback_deployment.xml  -jar /root/theia.jar --kafka=$Kafka_Broker --eps=$Events_Per_Second  --channels=$Channels_Per_Second --statSDHost=$StatSD_Host
 
